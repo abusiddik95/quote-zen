f0c85fe3973244c0971147d6f2504facb5593a66
abusiddik95 <siddikabu34@gmail.com>
2022-02-21 17:00:38 -0500

    onclick quote generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+  const [quotes, setQuotes] = useState('');
+  const getQuote = () =>{
+    fetch('https://type.fit/api/quotes')
+    .then(res => res.json())
+    .then(data => {
+      let randomNum = Math.floor(Math.random()* data.length);
+      setQuotes(data[randomNum])
+
+    });
+  };
+
+  useEffect(()=>{
+    getQuote();
+  },[]);
+
+    return (
+      <div className="App">
+        <div className="main-App">
+          <p>{quotes.text}</p>
+          <h4>{quotes.author}</h4>
+          <button onClick={getQuote} class="quote_btn">get quote</button>
+          <a href={'https://twitter.com/intent/tweet?text=${quote.text}'}>Tweet</a>
+        </div>
     </div>
-  );
+    );
 }
 
 export default App;
+
